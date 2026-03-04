26aefe96e86b985c2597801fff5ac89bae569125
LennartvdM <your-github-email@example.com>
2026-03-04 14:51:12 +0100

    initial commit - cloned framer site

---
+++ sites/2onvjkqnrbkhdnszrykaoo/chunk-ezmxfukt.mjs
@@ -0,0 +1,2 @@
+function t(e,a){return{description:"Empower your medical team to reach their full potential with video-based reflection and collaboration.",favicon:"https://framerusercontent.com/assets/9pFd7vvquDS5eplYFkw551yUBPs.png",robots:"max-image-preview:large",socialImage:"https://framerusercontent.com/assets/D5E2dEtF0UoDhvOzeXvMy3W1Zc.png",title:"Neoflix"}}export{t as a};
+//# sourceMappingURL=chunk-EZMXFUKT.mjs.map
